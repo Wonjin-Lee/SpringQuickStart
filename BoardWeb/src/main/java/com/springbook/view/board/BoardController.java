141796e30f55e7a0443a772d39f340f424ed403c
spacewhales <claude17@naver.com>
2019-01-21 23:28:06 +0900

    12th SQS commit : Business Component

--- BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
@@ -3,23 +3,30 @@ package com.springbook.view.board;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
-import com.springbook.biz.board.impl.BoardDAO;
 
 // 컨트롤러들을 하나로 통합
 @Controller
 @SessionAttributes("board")
 public class BoardController {
+	
+	// 컨트롤러들이 DAO 객체에 직접 접근하지 않고 비즈니스 컴포넌트를 사용해서 접근하도록 변환
+	// Autowired 어노테이션을 통해 자동적으로 BoardServiceImpl 객체가 의존성 주입된다.
+	@Autowired
+	private BoardService boardService;
+	
 	// 글 등록
 	@RequestMapping("/insertBoard.do")
-	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
-		boardDAO.insertBoard(vo);
+	public String insertBoard(BoardVO vo) {
+		boardService.insertBoard(vo);
 		return "redirect:getBoardList.do";
 	}
 	
@@ -27,22 +34,22 @@ public class BoardController {
 	// getBoard 비지니스 메소드가 호출될 때 Model 에 board 라는 이름으로 BoardVO 객체가 생성되는데
 	// SessionAttributes("board") 를 통해 세션에도 board 가 저장된다.
 	@RequestMapping("/updateBoard.do")
-	public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {
-		boardDAO.updateBoard(vo);
+	public String updateBoard(@ModelAttribute("board") BoardVO vo) {
+		boardService.updateBoard(vo);
 		return "getBoardList.do";
 	}
 	
 	// 글 삭제
 	@RequestMapping("/deleteBoard.do")
-	public String deleteBoard(BoardVO vo, BoardDAO boardDAO) {
-		boardDAO.deleteBoard(vo);
+	public String deleteBoard(BoardVO vo) {
+		boardService.deleteBoard(vo);
 		return "getBoardList.do";
 	}
 	
 	// 글 상세 조회
 	@RequestMapping("/getBoard.do")
-	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
-		model.addAttribute("board", boardDAO.getBoard(vo)); // Model 정보 저장
+	public String getBoard(BoardVO vo, Model model) {
+		model.addAttribute("board", boardService.getBoard(vo)); // Model 정보 저장
 		return "getBoard.jsp"; // View 이름 리턴
 	}
 	
@@ -57,9 +64,9 @@ public class BoardController {
 	
 	// 글 목록 검색
 	@RequestMapping("/getBoardList.do")
-	public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {
+	public String getBoardList(BoardVO vo, Model model) {
 		// Model 정보 저장
-		model.addAttribute("boardList", boardDAO.getBoardList(vo));
+		model.addAttribute("boardList", boardService.getBoardList(vo));
 		return "getBoardList.jsp"; // View 이름 리턴
 	}
 }
