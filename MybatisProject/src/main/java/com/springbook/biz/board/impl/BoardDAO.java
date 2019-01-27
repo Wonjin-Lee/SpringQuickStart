2c86a631df1c08d090b099a7bf32507d7d287764
spacewhales <claude17@naver.com>
2019-01-27 23:23:24 +0900

    16th SQS commit : Mybatis(3) - SQL Mapper

---
+++ MybatisProject/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -0,0 +1,40 @@
+package com.springbook.biz.board.impl;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.util.SqlSessionFactoryBean;
+
+// board-mapping.xml 에서 namespace 로 명시한 BoardDAO 의 각각의 태그 id 로 매칭
+public class BoardDAO {
+	private SqlSession mybatis;
+	
+	public BoardDAO() {
+		mybatis = SqlSessionFactoryBean.getSqlSessionInstance();
+	}
+	
+	public void insertBoard(BoardVO vo) {
+		mybatis.insert("BoardDAO.insertBoard", vo);
+		mybatis.commit();
+	}
+
+	public void updateBoard(BoardVO vo) {
+		mybatis.update("BoardDAO.updateBoard", vo);
+		mybatis.commit();
+	}
+	
+	public void deleteBoard(BoardVO vo) {
+		mybatis.delete("BoardDAO.deleteBoard", vo);
+		mybatis.commit();
+	}
+	
+	public BoardVO getBoard(BoardVO vo) {
+		return (BoardVO) mybatis.selectOne("BoardDAO.getBoard", vo);
+	}
+	
+	public List<BoardVO> getBoardList(BoardVO vo) {
+		return mybatis.selectList("BoardDAO.getBoardList", vo);
+	}
+}
